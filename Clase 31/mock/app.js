c139678a38a1e11c353e579a8c8fc4870c1c5a19
Diego <diego-jofre@live.com.ar>
2023-08-24 20:58:56 -0300

    mock and test

--- Clase 31/mock/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import userRouter from "./routes/users.router.js";
-
-const app = express();
-app.use("/api/users", userRouter);
-
-app.listen(3001, () => {
-  console.log("Server up");
-});
